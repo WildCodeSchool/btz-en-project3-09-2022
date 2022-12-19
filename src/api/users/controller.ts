683de4dfdf6ab510a628da10838538f52252a54d
LesCrow <john.baruteau@gmail.com>
2022-12-19 14:45:49 +0100

    create user route done

--- src/api/users/controller.ts
+++ src/api/users/controller.ts
@@ -1,9 +1,11 @@
 import getAll from "./handlers/getAll";
 import getOne from "./handlers/getOne";
+import create from "./handlers/create";
 
 const controller = {
   getAll,
   getOne,
+  create,
 };
 
 export default controller;
--- src/api/users/interface.ts
+++ src/api/users/interface.ts
@@ -2,18 +2,20 @@ import { User } from "@prisma/client";
 import { RequestHandler } from "express";
 import ResponseError from "../ResponseError";
 
-// type TAuthorBodyCreate = {
-//   imageUrl: string;
-//   firstname: string;
-//   lastname: string;
-//   workLocation: string;
-//   birthday: Date;
-//   email: string;
-//   hashedPassword: string;
-//   isDisabled: boolean;
-// };
+type TAuthorBodyCreate = {
+  imageUrl: string;
+  firstname: string;
+  lastname: string;
+  workLocation: string;
+  birthday: Date;
+  email: string;
+  hashedPassword: string;
+  teamId: string;
+  isDisabled: boolean;
+};
 
 export interface IUserHandlers {
   getAll: RequestHandler<null, User[] | ResponseError, null>;
   getOne: RequestHandler<{ id: string }, User | ResponseError, null>;
+  create: RequestHandler<null, User | ResponseError, TAuthorBodyCreate>;
 }

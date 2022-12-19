e233850e2e50f2cdf2c67bbf0b549eb00630f0aa
LesCrow <john.baruteau@gmail.com>
2022-12-19 14:54:55 +0100

    client.ts in prisma

---
+++ src/api/users/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import controller from "./controller";
+
+const router = Router();
+
+router.get("/", controller.getAll);
+router.get("/:id", controller.getOne);
+router.post("/", controller.create);
+router.put("/:id", controller.update);
+
+export default router;

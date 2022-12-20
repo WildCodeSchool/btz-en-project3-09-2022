243254149c9a86e44c7fff75bc16a1b263e1e8de
LesCrow <john.baruteau@gmail.com>
2022-12-20 14:30:32 +0100

    crud done on backup branch

---
+++ src/api/comments/interface.ts
@@ -0,0 +1,21 @@
+import { Comment } from "@prisma/client";
+import { RequestHandler } from "express";
+import ResponseError from "../ResponseError";
+
+type TCommentBodyCreate = {
+  content: string;
+  postId: string;
+  authorId: string;
+};
+
+export interface ICommentHandlers {
+  getAll: RequestHandler<null, Comment[] | ResponseError, null>;
+  getOne: RequestHandler<{ id: string }, Comment | ResponseError, null>;
+  create: RequestHandler<null, Comment | ResponseError, TCommentBodyCreate>;
+  update: RequestHandler<
+    { id: string },
+    Comment | ResponseError,
+    TCommentBodyCreate
+  >;
+  delete: RequestHandler<{ id: string }, Comment | ResponseError, null>;
+}
---
+++ src/api/comments/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import controller from "./controller";
+
+const router = Router();
+
+router.get("/", controller.getAll);
+router.get("/:id", controller.getOne);
+router.post("/", controller.create);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.delete_);
+
+export default router;

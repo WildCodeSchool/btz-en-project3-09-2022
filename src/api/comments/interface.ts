8e65151bb87156d62f3fc691f6641fbbb7b2afb6
e-genie <eugenie.froustey@gmail.com>
2023-01-20 12:09:08 +0100

    added postId to getAllComments

--- src/api/comments/interface.ts
+++ src/api/comments/interface.ts
@@ -4,8 +4,12 @@ import ResponseError from "../ResponseError";
 
 type TCommentBody = Omit<Comment, "id" | "createdAt" | "updatedAt">;
 
+type QueryParam = {
+  postId?: string;
+};
+
 export interface ICommentHandlers {
-  getAll: RequestHandler<null, Comment[] | ResponseError, null>;
+  getAll: RequestHandler<null, Comment[] | ResponseError, null, QueryParam>;
   getOne: RequestHandler<{ id: string }, Comment | ResponseError, null>;
   create: RequestHandler<null, Comment | ResponseError, TCommentBody>;
   update: RequestHandler<{ id: string }, Comment | ResponseError, TCommentBody>;

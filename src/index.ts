7f0757a44812a6d84aae2804aad54a826e8e0220
JIdayyy <julien_abbadie@hotmail.fr>
2023-01-30 09:57:16 +0100

    port as env variable 🙏

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import app from "./app";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

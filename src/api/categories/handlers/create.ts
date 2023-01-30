ee8264ec8049b1e5573f4a9554ecd9fbec5d974c
Tomahawkiwi <tlegendarme@gmail.com>
2023-01-30 16:02:40 +0100

    Added upload image to cloudinary in the create route of Category

--- src/api/categories/handlers/create.ts
+++ src/api/categories/handlers/create.ts
@@ -1,23 +1,39 @@
 /* eslint-disable no-console */
-
 import { CategoryHandlers } from "./../interface";
 import prisma from "../../../../prisma/client";
+import { uploadImage } from "../../../middlewares/upload/uploadCloudinary";
+import asyncFormParse from "../../../middlewares/upload/formParse";
 
 const createCategory: CategoryHandlers["create"] = async (req, res) => {
-  const { name, imageUrl, spaceId, ownerId } = req.body;
-  try {
-    const createCategory = await prisma.category.create({
-      data: {
-        name,
-        imageUrl,
-        spaceId,
-        ownerId,
-      },
-    });
-    res.status(200).json(createCategory);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: error });
+  const { fields, files } = await asyncFormParse(req);
+  const { name, spaceId, ownerId } = fields;
+
+  if (name[0] && spaceId[0] && ownerId[0] && files.categoryImage[0]) {
+    try {
+      const dataImage = await uploadImage(
+        files.categoryImage[0].path,
+        "/categoryImages"
+      );
+
+      if (dataImage) {
+        const createCategory = await prisma.category.create({
+          data: {
+            name: name[0],
+            imageUrl: dataImage.securePath,
+            spaceId: spaceId[0],
+            ownerId: ownerId[0],
+          },
+        });
+        res.status(200).json(createCategory);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error });
+    }
+  } else {
+    res
+      .status(500)
+      .json({ message: "Tous les champs ne sont pas correctement remplis" });
   }
 };
 

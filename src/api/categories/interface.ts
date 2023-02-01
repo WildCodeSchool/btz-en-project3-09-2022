ef4a1ea31a2401376d7159678e3c061c85ca6a36
Tomahawkiwi <tlegendarme@gmail.com>
2023-02-01 14:42:44 +0100

    Added iinclude Space for GetOne Category

--- src/api/categories/interface.ts
+++ src/api/categories/interface.ts
@@ -4,9 +4,16 @@ import ResponseError from "../ResponseError";
 
 type TCategoryBody = Omit<Category, "id" | "createdAt" | "updatedAt">;
 
+type TQueryCategory = { space: string };
+
 export interface CategoryHandlers {
   getAll: RequestHandler<null, Category[] | ResponseError, null>;
-  getOne: RequestHandler<{ id: string }, Category | ResponseError, null>;
+  getOne: RequestHandler<
+    { id: string },
+    Category | ResponseError,
+    null,
+    TQueryCategory
+  >;
   create: RequestHandler<
     { id: string },
     Category | ResponseError,

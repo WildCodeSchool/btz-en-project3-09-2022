ebff527d67f3d976827aab231f47fa666225e3e0
Tomahawkiwi <tlegendarme@gmail.com>
2023-02-07 14:09:41 +0100

    Removed checkDisabled for getOne category

--- src/api/categories/routes.ts
+++ src/api/categories/routes.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import checkIfIsAdminOrSuper from "../../middlewares/permissions/checkIfIsAdminOrSuper";
-import checkIfIsDisabledForGetOne from "../../middlewares/permissions/checkIfIsDisabledForGetOne";
 import checkIfIsOwner from "../../middlewares/permissions/checkIfIsOwner";
 import controller from "./controller";
 
 const router = Router();
 
 router.get("/", controller.getAll);
-router.get("/:id", checkIfIsDisabledForGetOne("category"), controller.getOne);
+router.get("/:id", controller.getOne);
 router.post("/", controller.create);
 router.delete("/:id", checkIfIsAdminOrSuper(), controller.delete);
 router.put("/:id", checkIfIsOwner("category"), controller.update);
